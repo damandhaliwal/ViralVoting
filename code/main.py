20d77114eddb19671a438aae09041f7a1321eb5a
Daman Dhaliwal <damandhaliwal1910@gmail.com>
2025-12-16 21:41:03 -0500

    final draft

--- code/main.py
+++ code/main.py
@@ -1,44 +1,55 @@
 # Replication code for ECO1465 Term Project
-from analysis import summary_statistics, treatment_covariate_balance_table, create_spillover_intensity_plot
+from analysis import summary_stats, guilt_analysis, balance_table, plot_spillover
 from lasso import run_lasso_analysis
 from ridge import run_ridge_analysis
 from sar import run_sar_analysis
-from ols import run_ols_regression, generate_regression_table
-from ml_methods import classification_tree, random_forest_classifier, run_model_comparison
+from ols import ols_analysis
+from ml_methods import run_model_comparison
 from dag import create_dag_plots
+from ipw import run_full_analysis
+from learners import main
+from doubly_robust import run_dr_with_interactions
+from double_ml import run_double_ml_analysis
 
 # print summary statistics of key variables - table 1
-summary_statistics()
+summary_stats()
 
-# table 2 - covariate balance treatment matrix
-treatment_covariate_balance_table()
+# table 2 - guilt analysis
+guilt_analysis()
 
-# figure 2 - DAG
+# table 3 - balance table
+balance_table()
+
+# figure 1 - DAG
 create_dag_plots()
+
+# figure 2 - spillover by treatment
+plot_spillover()
 
 # figure 3 - spillover by intensity
 create_spillover_intensity_plot()
 
-# regression table - OLS table 3
-model1 = run_ols_regression(outcome = 'voted', treatment_vars = ['treatment_self', 'treatment_civic duty', 'treatment_neighbors', 'treatment_hawthorne'])
-model2 = run_ols_regression(outcome = 'voted', treatment_vars = ['treatment_self', 'treatment_civic duty', 'treatment_neighbors', 'treatment_hawthorne'], control_vars = ['sex', 'yob', 'p2004'])
-model3 = run_ols_regression(outcome = 'voted', treatment_vars = ['treatment_self', 'treatment_civic duty', 'treatment_neighbors', 'treatment_hawthorne', 'treatment_intensity'], control_vars = ['sex', 'yob', 'p2004'])
-generate_regression_table(models = [model1, model2, model3],
-                          model_names=['Basic', 'Basic + Covariates', 'Diffusion'],
-                          filename = 'table3.tex')
+# regression table - OLS table 4
+ols_analysis()
 
-# spatial autoregressive model - table 4
+# table 5 - SAR results
 run_sar_analysis()
 
-# lasso and ridge plots - figure 4
-run_lasso_analysis()
-run_ridge_analysis()
+# figure 3a and 3b - Ridge and Lasso results
+run_ridge_analysis(plot_top_n=10)
+run_lasso_analysis(plot_top_n=10)
 
-# Machine Learning Results - produces figure 5
-classification_tree()
+#figure 4, 5 and table 6 - ensemble results
+run_model_comparison()
 
-# figure 6
-random_forest_classifier()
+# table 7 and 8 - IPW
+run_full_analysis()
 
-# table 5 - add include_gnn = true to run the Graphical Neural Net (will take a while to run)
-run_model_comparison(include_gnn = True)
+# table 9 and 10 - Learners
+main()
+
+# table 11 and 12 - Doubly Robust Methods
+run_dr_with_interactions()
+
+# table 13 and 14 - Double ML and Causal Forests
+run_double_ml_analysis()
